c89834908857dfb3f4adacf8dd4c732be5948c16
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-02-10 08:34:48 -0300

    feat: binary search

--- Algorithms/numbers.c
+++ Algorithms/numbers.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 
-int linearSearch(int number);
+void linearSearch(int number);
+void binarySearch(int number);
+int getMiddle(int length);
+void recursiveBinarySearch(int numbers[], int middle, int number, int numbersLength);
 
 int main(void)
 {
-  return linearSearch(0);
+  // linearSearch(0);
+  binarySearch(5);
+  return 0;
 }
 
-int linearSearch(int number)
+void linearSearch(int number)
 {
   int numbers[] = {4, 6, 8, 2, 7, 5, 0};
 
@@ -18,10 +25,108 @@ int linearSearch(int number)
     if (numbers[i] == number)
     {
       printf("Found at %d after %d!\n", i, count);
-      return 0;
     }
   }
 
   printf("Not found!\n");
-  return 1;
+}
+
+void binarySearch(int number)
+{
+  int numbers[] = {0, 1, 2, 3, 4, 5, 6};
+  int numbersLength = (int)(sizeof(numbers) / sizeof(numbers[0]));
+
+  int middle = getMiddle(numbersLength);
+  int numberAtMiddle = numbers[middle];
+
+  if (number == numberAtMiddle)
+  {
+    printf("Found!\n");
+  }
+  else
+  {
+    recursiveBinarySearch(numbers, middle, number, numbersLength);
+  }
+}
+
+void recursiveBinarySearch(int numbers[], int middle, int number, int numbersLength)
+{
+  if (numbersLength == 1)
+  {
+    if (numbers[0] == number)
+    {
+      printf("Found!\n");
+    }
+    else
+    {
+      printf("Not found!\n");
+    }
+    return;
+  }
+
+  int newArraySize = numbersLength - middle;
+
+  if (number > numbers[middle])
+  {
+    int rightHalf[newArraySize];
+    int index = 0;
+    for (int i = middle; i < numbersLength; i++)
+    {
+      rightHalf[index] = numbers[i];
+      printf("Right Half Array at [%d] = %d\n", index, numbers[i]);
+      index++;
+    }
+
+    middle = getMiddle(newArraySize);
+    printf("Right Half length is [%d] Middle is [%d]\n", newArraySize, middle);
+    if (number == rightHalf[middle])
+    {
+      printf("Found right!\n");
+    }
+    else
+    {
+      recursiveBinarySearch(rightHalf, middle, number, newArraySize);
+    }
+  }
+  else
+  {
+    int leftHalf[newArraySize];
+    for (int i = 0; i < middle; i++)
+    {
+      leftHalf[i] = numbers[i];
+      printf("Left Half Array at [%d] = %d\n", i, numbers[i]);
+    }
+
+    middle = getMiddle(newArraySize);
+    printf("Right Half length is [%d] Middle is [%d]\n", newArraySize, middle);
+    if (number == leftHalf[middle])
+    {
+      printf("Found left!\n");
+    }
+    else
+    {
+      recursiveBinarySearch(leftHalf, middle, number, newArraySize);
+    }
+  }
+}
+
+int getMiddle(int length)
+{
+  bool IS_EVEN = false;
+  if (length % 2 == 0)
+  {
+    IS_EVEN = true;
+  }
+
+  int middle = 0;
+  if (IS_EVEN)
+  {
+    middle = length / 2;
+  }
+  else
+  {
+    middle = floor(length / 2);
+  }
+
+  return middle;
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CS50x-2022
 Studies following [CS50x 2022](https://cs50.harvard.edu/x/2022/)
+
+## Compiling
+make <>.c
+
+## Run
+./<>
