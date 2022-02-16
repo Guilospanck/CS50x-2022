06cb5e5274cc4d10ba8fbd643bf3ae567b672736
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-02-16 05:49:33 -0300

    feat: dynamic memory allocation. malloc and free

---
+++ Memory/dynamicAllocation.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Malloc (memory allocation) and free
+int main(void)
+{
+
+  char *s = "hi!";
+  char *t = malloc(strlen(s) + 1); // length of the string plus one for the "\0" (trailing null character)
+  if (t == NULL)
+  {
+    return 1;
+  }
+
+  // one option to copy str
+  // for (int i = 0, n = strlen(s) + 1; i < n; i++)
+  // {
+  //   t[i] = s[i];
+  // }
+
+  // another
+  strcpy(t, s);
+
+  if (strlen(t) > 0)
+  {
+    t[0] = toupper(t[0]);
+  }
+
+  printf("s: %s\n", s);
+  printf("t: %s\n", t);
+
+  // frees memory
+  free(t);
+}
